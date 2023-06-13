691c943a1a587d0c827ccfa57ad40195c982da8d
Jerusha <62611035+CelestialJerusha@users.noreply.github.com>
2023-06-13 23:26:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,10 +6,8 @@ const person = {
 };
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
-const team = [];
-players.forEach(function (item) {
-        team.push(item);
-    });
+
+
 
 const team1 = players.slice();
 
